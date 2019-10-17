68404ed1e704b76f226e042ea53167cbe74e6228
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-10-16 21:34:28 -0400

    needs more work

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       System.out.println(WordWrap.wrapText("this is only a test hopefully this works please",13));
     }
 }
--- src/WordWrap.java
+++ src/WordWrap.java
@@ -1,4 +1,21 @@
 public class WordWrap {
 
 
+    public static String wrapText(String words, int length) {
+        String[] splitWords = words.split(" ");
+        StringBuilder wordStack = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        for (String word:splitWords) {
+            temp.append(word).append(" ");
+            if(temp.length() >= length){
+                wordStack.append(temp).append("\n");
+                temp.delete(0,temp.length());
+            }else{
+
+            }
+
+
+        }
+        return wordStack.toString();
+    }
 }
